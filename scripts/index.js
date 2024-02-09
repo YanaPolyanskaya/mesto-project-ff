72ec925e5c740053faed5bf4ae71d6ebfb68321f
Yana Polyanskaya <polyanskayayanina@yandex.ru>
2024-02-09 15:48:02 +0300

    код готов. 1 проверка

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// import "./cards.js";
-
 // @todo: Темплейт карточки
 
 const cardTemplate = document.querySelector("#card-template").content;
@@ -13,23 +11,23 @@ const placesList = document.querySelector(".places__list");
 function renderCards(imageName, imageLink) {
   const card = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = card.querySelector(".card__image");
-  const cardDeleteButton = card.querySelector("card__delete-button");
-  const cardDescription = card.querySelector("card__description");
-  const cardTitle = cardDescription.querySelector("card__title");
-  const cardLikeButton = cardDescription.querySelector("card__like-button");
+  const cardDeleteButton = card.querySelector(".card__delete-button");
+  const cardDescription = card.querySelector(".card__description");
+  const cardTitle = card.querySelector(".card__title");
+  const cardLikeButton = card.querySelector(".card__like-button");
 
-  cardImage.src = `url(${imageLink})`;
+  cardImage.src = imageLink;
   cardTitle.textContent = imageName;
 
   // @todo: Функция удаления карточки
 
-  removeCard.addEventListener("click", () => {
-    card.removeCard();
+  cardDeleteButton.addEventListener("click", () => {
+    card.remove();
   });
 
   return card;
 }
-renderCards();
+
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((card) =>
